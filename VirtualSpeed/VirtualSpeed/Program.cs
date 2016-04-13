8d536f7dddb602a597b2bea44462b583ca97741b
Simone Chiaretta <simone.chiaretta@consilium.europa.eu>
2016-04-13 17:05:36 +0200

    Implemented convert of tcx file based on physics

---
+++ VirtualSpeed/VirtualSpeed/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace VirtualSpeed
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var calc = new VirtualSpeedCalculator();
+
+            XmlDocument myXmlDocument = new XmlDocument();
+            myXmlDocument.Load("powerData.tcx");
+
+            XmlNode trainingNode;
+            trainingNode = myXmlDocument.DocumentElement;
+
+            double cumulatedDistance = 0;
+            DateTime prevTime = DateTime.MinValue;
+
+            foreach (XmlNode activitiesNode in trainingNode.ChildNodes)
+                foreach (XmlNode activityNode in activitiesNode.ChildNodes)
+                    foreach (XmlNode lapNode in activityNode.ChildNodes)
+                    {
+                        if (lapNode.Name == "Lap")
+                        {
+                            XmlNode lapDistance = null;
+                            XmlNode lapMaxSpeedNode = null;
+                            XmlNode lapAvgSpeed = null;
+                            XmlNode lapTime = null;
+
+                            double lapCumulativeDistance = 0;
+                            double lapMaxSpeed = 0;
+
+                            foreach (XmlNode track in lapNode.ChildNodes)
+                            {
+                                if (track.Name == "DistanceMeters") {
+                                    lapDistance = track;
+                                }
+                                else if (track.Name == "TotalTimeSeconds")
+                                {
+                                    lapTime = track;
+                                }
+                                else if (track.Name == "MaximumSpeed")
+                                {
+                                    lapMaxSpeedNode = track;
+                                }
+                                else if (track.Name == "Extensions")
+                                {
+                                    foreach (XmlNode ext in track.ChildNodes)
+                                        foreach (XmlNode LX in ext.ChildNodes)
+                                            if (LX.Name == "AvgSpeed")
+                                                lapAvgSpeed = LX;
+                                }
+                                else if (track.Name == "Track")
+                                    foreach (XmlNode point in track.ChildNodes)
+                                    {
+                                        XmlNode distance = null;
+                                        XmlNode time = null;
+                                        XmlNode watt = null;
+                                        XmlNode speed = null;
+                                        foreach (XmlNode value in point.ChildNodes)
+                                        {
+                                            if (value.Name == "Time")
+                                                time = value;
+                                            else if (value.Name == "DistanceMeters")
+                                                distance = value;
+                                            else if (value.Name == "Extensions")
+                                            {
+                                                foreach (XmlNode ext in value.ChildNodes)
+                                                    foreach (XmlNode tcxValue in ext.ChildNodes)
+                                                    {
+                                                        if (tcxValue.Name == "ns3:Watts")
+                                                            watt = tcxValue;
+                                                        else if (tcxValue.Name == "ns3:Speed")
+                                                            speed = tcxValue;
+                                                    }
+                                            }
+                                        }
+
+                                        var newSpeed = calc.CalculateVelocity(Double.Parse(watt.InnerText));
+                                        var newSpeedMS = calc.ConvertKmhToMS(newSpeed);
+                                        lapMaxSpeed = Math.Max(lapMaxSpeed, newSpeedMS);
+
+                                        var pointDistance = calc.CalculateDistance(newSpeed, 1);
+                                        cumulatedDistance = cumulatedDistance + pointDistance;
+                                        lapCumulativeDistance = lapCumulativeDistance + pointDistance;
+
+                                        distance.InnerText = cumulatedDistance.ToString(new CultureInfo("en-US"));
+                                        speed.InnerText = newSpeedMS.ToString(new CultureInfo("en-US"));
+                                    }
+                            }
+
+                            lapDistance.InnerText= lapCumulativeDistance.ToString(new CultureInfo("en-US"));
+                            lapMaxSpeedNode.InnerText=lapMaxSpeed.ToString(new CultureInfo("en-US"));
+
+                            var avg = lapCumulativeDistance / Double.Parse(lapTime.InnerText);
+
+                            lapAvgSpeed.InnerText = avg.ToString(new CultureInfo("en-US"));
+
+                        }
+                    }
+            myXmlDocument.Save("Fixed.tcx");
+
+        }
+
+        private void Test(string[] args)
+        {
+            var what = args[0];
+
+            var calc = new VirtualSpeedCalculator();
+
+            if (what.Equals("P"))
+            {
+                var speed = Double.Parse(args[1]);
+
+                var pwr = calc.CalculatePower(speed);
+                Console.WriteLine("Measuring Power needed to go to a given speed");
+                Console.WriteLine("{0} Km/h -> {1}W", speed, pwr);
+            }
+            else if (what.Equals("V"))
+            {
+                var pwr = Double.Parse(args[1]);
+
+                var speed = calc.CalculateVelocity(pwr);
+                Console.WriteLine("Measuring Speed obtained for a given power");
+                Console.WriteLine("{0} W -> {1} Km/h", pwr, speed);
+            }
+
+            else if (what.Equals("D"))
+            {
+                var pwr = Double.Parse(args[1]);
+                var time = Double.Parse(args[2]);
+
+                var speed = calc.CalculateVelocity(pwr);
+                var distance = calc.CalculateDistance(speed, time);
+                Console.WriteLine("Measuring Distance for given power");
+                Console.WriteLine("{0} W for {1} sec = {2} meters", pwr, time, distance);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+
+
+}
---
+++ VirtualSpeed/VirtualSpeed/VirtualSpeedCalculator.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualSpeed
+{
+    
+    public class VirtualSpeedCalculator
+    {
+        private const double G = 9.8067;
+
+        public VirtualSpeedCalculator(): this(new Parameters())
+        {
+
+        }
+
+        public VirtualSpeedCalculator(Parameters parameters)
+        {
+            Parameters = parameters;
+        }
+
+        public Parameters Parameters { get; set; }
+
+
+        public double CalculatePower(double velocityKmh)
+        {
+            Forces forces = CalculateForces(velocityKmh);
+
+            double wheelPower = forces.Total * (velocityKmh * 1000.0 / 3600.0);
+
+            double legPower = wheelPower / (1.0 - (Parameters.DriveTrainLoss / 100.0));
+
+            return legPower;
+        }
+
+        private Forces CalculateForces(double velocityKmh)
+        {
+            var forces = new Forces();
+            var velocityMS = ConvertKmhToMS(velocityKmh);
+
+            forces.Gravity = G * Parameters.WeightTotal * Math.Sin(Math.Atan(Parameters.ClimbGrade / 100));
+
+            forces.Rolling = G * Parameters.WeightTotal * Math.Cos(Math.Atan(Parameters.ClimbGrade / 100)) * Parameters.Crr;
+
+            forces.Drag = 0.5 * Parameters.GetCdA() * Parameters.Rho * velocityMS * velocityMS;
+
+
+            return forces;
+        }
+
+        public double ConvertKmhToMS(double velocityKmh)
+        {
+            return velocityKmh * 1000 / 3600;
+        }
+
+        public double CalculateDistance(double speedKmh, double timeSec)
+        {
+            return ConvertKmhToMS(speedKmh) * timeSec;
+        }
+
+        public double CalculateVelocity(double power)
+        {
+            var epsilon = 0.000001;
+            var lowervel = -1000.0;
+            var uppervel = 1000.0;
+            var midvel = 0.0;
+
+            var midpow = CalculatePower(midvel);
+
+            var itcount = 0;
+            do
+            {
+                if (Math.Abs(midpow - power) < epsilon)
+                    break;
+
+                if (midpow > power)
+                    uppervel = midvel;
+                else
+                    lowervel = midvel;
+
+                midvel = (uppervel + lowervel) / 2.0;
+                midpow = CalculatePower(midvel);
+            } while (itcount++ < 100);
+
+            return midvel;
+        }
+    }
+
+    public class Parameters
+    {
+        public Parameters()
+        {
+            DriveTrainLoss = 3.0;
+            WeightRider = 83;
+            WeightBike = 8;
+            ClimbGrade = 0;
+            Crr = 0.005;
+            A = 0.509;
+            Cd = 0.63;
+            Rho = 1.226;
+            CdA = 0;
+        }
+
+        public double DriveTrainLoss { get; set; }
+        public double WeightRider { get; set; }
+        public double WeightBike { get; set; }
+
+        public double ClimbGrade { get; set; }
+
+        public double Crr { get; set; }
+
+        public double WeightTotal
+        {
+            get
+            {
+                return WeightRider + WeightBike;
+            }
+        }
+
+        public double A { get; set; }
+        public double Cd { get; set; }
+        public double Rho { get; set; }
+        public double CdA { get; set; }
+
+        public double GetCdA()
+        {
+            if (CdA == 0) return Cd * A;
+            return CdA;
+        }
+    }
+
+    internal class Forces
+    {
+        public double Gravity { get; set; }
+        public double Rolling { get; set; }
+        public double Drag { get; set; }
+
+        public double Total
+        {
+            get
+            {
+                return Gravity + Rolling + Drag;
+            }
+        }
+
+    }
+
+}
